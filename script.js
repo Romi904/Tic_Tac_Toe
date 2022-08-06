07c3a4ca30b1a14b7ab254532a32925de5262e06
Romijul <romilaskar904@gmail.com>
2022-08-06 23:57:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+let z = "X";
+let p = 9;
+let audioTurn = new Audio("ting.mp3");
+let winner = new Audio("applause.mp3");
+function change()
+{
+    if(z=='X')
+    {
+        z='O';
+    }
+    else{
+        z='X';
+    }
+}
+function namee(){
+    if(z=='X')
+    {
+        document.getElementById('h2').innerText="Your turn";
+    }
+    else{
+        document.getElementById('h2').innerText="Opponent turn";
+    }
+}
+function clic(m) {
+    audioTurn.play();
+    document.getElementById(m).innerHTML = z;
+    p--;
+    change();
+    checkwin();
+    namee();
+    draw();
+}
+function began(){
+    document.getElementById('banner').style.visibility="hidden";
+    namee();
+    openFullscreen();
+}
+function checkwin() {
+    var a1, a2, a3, b1, b2, b3, c1, c2, c3;
+    a1 = document.getElementById("1").innerText;
+    a2 = document.getElementById("2").innerText;
+    a3 = document.getElementById("3").innerText;
+    b1 = document.getElementById("4").innerText;
+    b2 = document.getElementById("5").innerText;
+    b3 = document.getElementById("6").innerText;
+    c1 = document.getElementById("7").innerText;
+    c2 = document.getElementById("8").innerText;
+    c3 = document.getElementById("9").innerText;
+
+    if((a1=='O'&&a2=='O'&&a3=='O')||(b1=='O'&&b2=='O'&&b3=='O')||(c1=='O'&&c2=='O'&&c3=='O')||(a1=='O'&&b1=='O'&&c1=='O')||(b2=='O'&&a2=='O'&&c2=='O')||(a3=='O'&&b3=='O'&&c3=='O')||(a1=='O'&&b2=='O'&&c3=='O')||(a3=='O'&&b2=='O'&&c1=='O'))
+    {
+        winner.play();
+        window.confirm("Winner");
+        window.location.reload();
+    }
+    if((a1=='X'&&a2=='X'&&a3=='X')||(b1=='X'&&b2=='X'&&b3=='X')||(c1=='X'&&c2=='X'&&c3=='X')||(a1=='X'&&b1=='X'&&c1=='X')||(b2=='X'&&a2=='X'&&c2=='X')||(a3=='X'&&b3=='X'&&c3=='X')||(a1=='X'&&b2=='X'&&c3=='X')||(a3=='X'&&b2=='X'&&c1=='X'))
+    {
+        winner.play();
+        window.confirm("Winner");
+        window.location.reload();
+    }
+}
+function draw(){
+    if(p<1)
+    {
+        document.getElementById('banner').style.visibility="visible";
+        document.getElementById('banner').innerText="-----DRAW-----";
+        setTimeout(miu,1000);
+    }
+}
+function miu(){
+    window.location.reload();
+}
+function openFullscreen() {
+    var elem=document.getElementById('body');
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
